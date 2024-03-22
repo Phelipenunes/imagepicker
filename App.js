7f985c65c72ef91b215967e88f178287a28c0586
Phelipe <phelipesenac@gmail.com>
2024-03-22 09:49:18 -0300

    .
    .

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
+import { useState, useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
+  const [foto, setFoto] = useState(null);
+  const [status, requestPermission] = ImagePicker.useCameraPermissions();
+
+  console.log(status);
+
+  useEffect(() => {
+    async function verificarPermissoes() {
+      const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+      requestPermission(cameraStatus === "granted");
+    }
+    verificarPermissoes();
+  }, []);
+
+  const escolherFoto = async () => {
+    const resultado = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!resultado.canceled) {
+      setFoto(resultado.assets[0].uri);
+    }
+  };
+  console.log(foto);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={styles.container}>
+        <Button onPress={escolherFoto} title="Escolher foto" />
+        {foto ? (
+          <Image source={{ uri: foto }} style={styles.image} />
+        ) : (
+          <Text>Você ainda não escolheu uma foto!</Text>
+        )}
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 300,
+    height: 300,
   },
 });
