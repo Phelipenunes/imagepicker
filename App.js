9751d5552647f63b00b5139d384362f881dbb608
Phelipe <phelipesenac@gmail.com>
2024-03-22 10:33:58 -0300

    .
    .

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View, Image } from "react-native";
 import { useState, useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
+import * as MediaLibrary from "expo-media-library";
 
 export default function App() {
   const [foto, setFoto] = useState(null);
@@ -29,11 +30,24 @@ export default function App() {
       setFoto(resultado.assets[0].uri);
     }
   };
-  console.log(foto);
+
+  const acessarCamera = async () => {
+    const imagem = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!imagem.canceled) {
+      await MediaLibrary.saveToLibraryAsync(image.assets[0].uri);
+      setFoto(imagem.assets[0].uri);
+    }
+  };
+
   return (
     <>
       <StatusBar />
       <View style={styles.container}>
+        <Button onPress={acessarCamera} title="Tirar uma foto" />
         <Button onPress={escolherFoto} title="Escolher foto" />
         {foto ? (
           <Image source={{ uri: foto }} style={styles.image} />
